fad4caacc6aa29b07f242b927cb105fd26a52fe7
Jair Partida <jairpartida1@gmail.com>
2022-10-01 21:45:00 -0600

    project done

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,3 +1,4 @@
+import { Fragment, useCallback, useRef } from 'react'
 import useQuoter from "../hooks/useQuoter"
 import { BRANDS, PLANS } from "../constants"
 
@@ -5,11 +6,15 @@ const Result = () => {
 
     const { result, datos } = useQuoter()
     const { brand, plan, year } = datos
+    const brandRef =useRef(brand)
+    const yearRef = useRef(year)
 
-    const [nameBrand] = BRANDS.filter(b => b.id === Number(brand))
-    const [namePlan] = PLANS.filter(p => p.id === Number(plan))
+    // const [nameBrand] = BRANDS.filter(b => b.id === Number(brand))
 
-    console.log("BRAND: " + brand)
+    const [namePlan] = useCallback( 
+        PLANS.filter(p => p.id === Number(plan)),
+        [result]
+    )
 
     if(result === 0) return null
 
@@ -18,21 +23,25 @@ const Result = () => {
         <h2 className="text-gray-600 font-bold text-2xl font-figtree">
             Resume
         </h2>
-
-        <p className="my-2">
-            <span className="font-figtree font-semibold text-lg text-gray-700">Brand: </span>
-            {brand}
-        </p>
+        <Fragment>
+            <p className="my-2">
+                <span className="font-figtree font-semibold text-lg text-gray-700">Brand: </span>
+                {brandRef.current}
+            </p>
+        </Fragment>
+        
 
         <p className="my-2">
             <span className="font-figtree font-semibold text-lg text-gray-700">Plan: </span>
             {namePlan.name}
         </p>
-
-        <p className="my-2">
-            <span className="font-figtree font-semibold text-lg text-gray-700">Car year: </span>
-            {year}
-        </p>
+        <Fragment>
+            <p className="my-2">
+                <span className="font-figtree font-semibold text-lg text-gray-700">Car year: </span>
+                {yearRef.current}
+            </p>
+        </Fragment>
+        
 
         <p className="my-2 text-2xl">
             <span className="font-figtree font-semibold text-gray-700">Total: </span>
